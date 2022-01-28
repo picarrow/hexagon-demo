ce68d99cd9e300080ba76859b7bcc908d7726cd8
Daniil Novikov <dannovikov@optonline.net>
2022-01-27 21:33:04 -0500

    Javafx Hexagon

---
+++ src/Hexagon.java
@@ -0,0 +1,20 @@
+//Author: Daniil Novikov
+//Hexagon
+//Description: Prefabed Hexagon inheriting from Javafx Polygon
+import javafx.scene.shape.Polygon;
+public class Hexagon extends Polygon{
+    private double centerX, centerY;
+    private final double radius = 10;//radius of circle hexagon is inscribed in
+    public Hexagon(double x, double y)
+    {
+        centerX = x;
+        centerY = y;
+        Double [] p = new Double[12];
+        for(int i = 0; i<6;i++)
+        {
+            p[2*i] = centerX+radius*Math.cos(Math.toRadians(45+i*45));
+            p[2*i+1] = centerY+radius*Math.sin(Math.toRadians(45+i*45));
+        }
+        getPoints().addAll(p);
+    }
+}
