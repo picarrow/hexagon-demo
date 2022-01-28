77a5c82405fbc6366f5b5a87a56b34cb8f464fad
Daniil Novikov <dannovikov@optonline.net>
2022-01-27 22:20:27 -0500

    Javafx

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,57 @@
+//Hexagon Grid
+//Author: Guiseppe Guerini - Main structure
+//Additional Contributor: Daniil Novikov - Javafx
+
+//Datastructure and filereading imports
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+//Javafx and graphics imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class Main
+public class Main extends Application
 {
     /***
      * Uses a BufferedReader to read hex coordinates, then transforms
      * those coordinates into hexes.
     ***/
+	
+    public void start(Stage primaryStage)
+    {
+	
+	Set<Hex> hexes = null;
+
+        try
+        {
+            hexes = hexesFrom("hex-example-1.txt");
+        }
+        catch(Exception e)
+        {
+            return;
+        }
+
+        HexGraph graph = new HexGraph(hexes);
+        Hex src = new Hex(0, 2);
+        Hex dst = new Hex(2, 0);
+        List<Hex> path = graph.getPath(src, dst);
+
+        System.out.println("Hexes: " + hexes);
+        System.out.println("Graph: " + graph);
+        System.out.println("Path (H02 -> H20): " + path);
+
+
+        primaryStage.setTitle("JavaFX Experiment");//Window
+        Pane pane = new Pane();//Pane contained in scene
+        Scene scene = new Scene(pane, 1360, 700);//Set scene size
+        primaryStage.setScene(scene);//Add scene to window
+        primaryStage.show();//Display window
+    }
     private static Set<Hex> hexesFrom(
         String fileName)
         throws IOException
@@ -45,27 +86,9 @@ public class Main
         return hexes;
     }
 
-    public static void main(
-        String[] args)
+    public static void main(String[] args)
     {
-        Set<Hex> hexes = null;
-
-        try
-        {
-            hexes = hexesFrom("hex-example-1.txt");
-        }
-        catch(Exception e)
-        {
-            return;
-        }
-
-        HexGraph graph = new HexGraph(hexes);
-        Hex src = new Hex(0, 2);
-        Hex dst = new Hex(2, 0);
-        List<Hex> path = graph.getPath(src, dst);
-
-        System.out.println("Hexes: " + hexes);
-        System.out.println("Graph: " + graph);
-        System.out.println("Path (H02 -> H20): " + path);
+        launch(args);
+	System.exit(0);
     }
 }
