44c20c7f23ccf54817a1927a13708e6227861992
gguerini129 <gguerini999@gmail.com>
2022-02-09 03:15:13 -0500

    Fixed an issue with LSP
    Removed the polymorphic behavior of AxialHexPoint and DoubledHexPoint in order to adhere to LSP (Liskov Substitution Principle).

--- app/src/main/java/AxialHexPoint.java
+++ app/src/main/java/AxialHexPoint.java
@@ -1,11 +1,48 @@
 public class AxialHexPoint
-    extends HexPoint
 {
+    private final int x, y;
+
     public AxialHexPoint(
         int x,
         int y)
     {
-        super(x, y);
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    @Override
+    public boolean equals(
+        Object o)
+    {
+        return o != null
+            && o instanceof AxialHexPoint
+            && this.x == ((AxialHexPoint)o).x
+            && this.y == ((AxialHexPoint)o).y;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 17;
+        hash = 31 * hash + x;
+        hash = 31 * hash + y;
+        return hash;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + x + " " + y + ")";
     }
 
     public DoubledHexPoint toDoubled()
--- app/src/main/java/DoubledHexPoint.java
+++ app/src/main/java/DoubledHexPoint.java
@@ -1,11 +1,48 @@
 public class DoubledHexPoint
-    extends HexPoint
 {
+    private final int x, y;
+
     public DoubledHexPoint(
         int x,
         int y)
     {
-        super(x, y);
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    @Override
+    public boolean equals(
+        Object o)
+    {
+        return o != null
+            && o instanceof DoubledHexPoint
+            && this.x == ((DoubledHexPoint)o).x
+            && this.y == ((DoubledHexPoint)o).y;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 17;
+        hash = 31 * hash + x;
+        hash = 31 * hash + y;
+        return hash;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + x + " " + y + ")";
     }
 
     public AxialHexPoint toAxial()
